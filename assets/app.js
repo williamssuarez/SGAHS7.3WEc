aef4a350809ab77b8c1715e6e881cc97d728b0fb
williamssuarez <williamssuarez148@gmail.com>
2025-12-12 06:32:34 -0400

    app.js fix (idk what was wrong, I just reverted the file)

--- assets/app.js
+++ assets/app.js
@@ -89,156 +89,6 @@ function softDeleteRecord(deleteUrl, csrfToken) {
 window.softDeleteRecord = softDeleteRecord;
 /* SWEETALERTS END */
 
-new Sortable(document.querySelector('.connectedSortable'), {
-    group: 'shared',
-    handle: '.card-header',
-});
-
-const cardHeaders = document.querySelectorAll('.connectedSortable .card-header');
-cardHeaders.forEach((cardHeader) => {
-    cardHeader.style.cursor = 'move';
-});
-
-/* Overlayscrollbars config start */
-const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
-
-//ALL DOMCONTENTLOADED EVENTS GO HERE
-document.addEventListener('DOMContentLoaded', function () {
-    //OverlayScrollbars
-    const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
-    if (sidebarWrapper) {//check
-        OverlayScrollbars(sidebarWrapper, {
-            scrollbars: {
-                theme: 'os-theme-light',
-                autoHide: 'leave',
-                clickScroll: true,
-            },
-        });
-    }
-
-    //fullcalendar
-    /*var calendarEl = document.getElementById('calendar');
-    if (calendarEl) {
-        var calendar = new Calendar(calendarEl, {
-            // 1. Register the plugins you imported
-            plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin ],
-            locales: [ esLocale ],
-
-            // 2. Set the initial view and other options
-            initialView: 'dayGridMonth',
-            headerToolbar: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            },
-            locale: 'es',
-
-            // 3. Add sample data
-            events: [
-                { title: 'Meeting', start: '2025-11-14' },
-                { title: 'Project Deadline', start: '2025-11-20', end: '2025-11-22' }
-            ]
-        });
-
-        // 4. Render the calendar
-        calendar.render();
-    }*/
-});
-/* Overlayscrollbars config end */
-
-// World map by jsVectorMap
-new jsVectorMap({
-    selector: '#world-map',
-    map: 'world',
-});
-
-// Sparkline charts
-const option_sparkline1 = {
-    series: [
-        {
-            data: [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021],
-        },
-    ],
-    chart: {
-        type: 'area',
-        height: 50,
-        sparkline: {
-            enabled: true,
-        },
-    },
-    stroke: {
-        curve: 'straight',
-    },
-    fill: {
-        opacity: 0.3,
-    },
-    yaxis: {
-        min: 0,
-    },
-    colors: ['#DCE6EC'],
-};
-
-const sparkline1 = new ApexCharts(document.querySelector('#sparkline-1'), option_sparkline1);
-sparkline1.render();
-
-const option_sparkline2 = {
-    series: [
-        {
-            data: [515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921],
-        },
-    ],
-    chart: {
-        type: 'area',
-        height: 50,
-        sparkline: {
-            enabled: true,
-        },
-    },
-    stroke: {
-        curve: 'straight',
-    },
-    fill: {
-        opacity: 0.3,
-    },
-    yaxis: {
-        min: 0,
-    },
-    colors: ['#DCE6EC'],
-};
-
-const sparkline2 = new ApexCharts(document.querySelector('#sparkline-2'), option_sparkline2);
-sparkline2.render();
-
-const option_sparkline3 = {
-    series: [
-        {
-            data: [15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21],
-        },
-    ],
-    chart: {
-        type: 'area',
-        height: 50,
-        sparkline: {
-            enabled: true,
-        },
-    },
-    stroke: {
-        curve: 'straight',
-    },
-    fill: {
-        opacity: 0.3,
-    },
-    yaxis: {
-        min: 0,
-    },
-    colors: ['#DCE6EC'],
-};
-
-const sparkline3 = new ApexCharts(document.querySelector('#sparkline-3'), option_sparkline3);
-sparkline3.render();
-
-//no toggler
-
 $(document).ready(function () {
     $('.dtHere').DataTable({
         pageLength: 5,
@@ -405,9 +255,240 @@ $(document).ready(function () {
 
     // Optional: Keep the current index updated if the tab is changed externally
     tabList.on('shown.bs.tab', function (e) {
-        const targetId = $(e.target).attr('id'); // e.g., "tab2" or "tab4"
+        const targetId = $(e.target).attr('id'); // e.g., "tab2"
         currentTabIndex = tabList.index(e.target);
         updateNavButtons();
     });
     // ---End of Event Listeners for tabs ---
 });
+
+new Sortable(document.querySelector('.connectedSortable'), {
+    group: 'shared',
+    handle: '.card-header',
+});
+
+const cardHeaders = document.querySelectorAll('.connectedSortable .card-header');
+cardHeaders.forEach((cardHeader) => {
+    cardHeader.style.cursor = 'move';
+});
+
+/* Overlayscrollbars config start */
+const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
+
+//ALL DOMCONTENTLOADED EVENTS GO HERE
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('IT LOADS CORRECTLY SO IDK WHATS WRONG');
+    //OverlayScrollbars
+    const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+    if (sidebarWrapper) {//check
+        OverlayScrollbars(sidebarWrapper, {
+            scrollbars: {
+                theme: 'os-theme-light',
+                autoHide: 'leave',
+                clickScroll: true,
+            },
+        });
+    }
+
+    //fullcalendar
+    var calendarEl = document.getElementById('calendar');
+    if (calendarEl) {
+        var calendar = new Calendar(calendarEl, {
+            // 1. Register the plugins you imported
+            plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin ],
+            locales: [ esLocale ],
+
+            // 2. Set the initial view and other options
+            initialView: 'dayGridMonth',
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+            locale: 'es',
+
+            // 3. Add sample data
+            events: [
+                { title: 'Meeting', start: '2025-11-14' },
+                { title: 'Project Deadline', start: '2025-11-20', end: '2025-11-22' }
+            ]
+        });
+
+        // 4. Render the calendar
+        calendar.render();
+    }
+});
+/* Overlayscrollbars config end */
+
+// World map by jsVectorMap
+new jsVectorMap({
+    selector: '#world-map',
+    map: 'world',
+});
+
+// Sparkline charts
+const option_sparkline1 = {
+    series: [
+        {
+            data: [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021],
+        },
+    ],
+    chart: {
+        type: 'area',
+        height: 50,
+        sparkline: {
+            enabled: true,
+        },
+    },
+    stroke: {
+        curve: 'straight',
+    },
+    fill: {
+        opacity: 0.3,
+    },
+    yaxis: {
+        min: 0,
+    },
+    colors: ['#DCE6EC'],
+};
+
+const sparkline1 = new ApexCharts(document.querySelector('#sparkline-1'), option_sparkline1);
+sparkline1.render();
+
+const option_sparkline2 = {
+    series: [
+        {
+            data: [515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921],
+        },
+    ],
+    chart: {
+        type: 'area',
+        height: 50,
+        sparkline: {
+            enabled: true,
+        },
+    },
+    stroke: {
+        curve: 'straight',
+    },
+    fill: {
+        opacity: 0.3,
+    },
+    yaxis: {
+        min: 0,
+    },
+    colors: ['#DCE6EC'],
+};
+
+const sparkline2 = new ApexCharts(document.querySelector('#sparkline-2'), option_sparkline2);
+sparkline2.render();
+
+const option_sparkline3 = {
+    series: [
+        {
+            data: [15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21],
+        },
+    ],
+    chart: {
+        type: 'area',
+        height: 50,
+        sparkline: {
+            enabled: true,
+        },
+    },
+    stroke: {
+        curve: 'straight',
+    },
+    fill: {
+        opacity: 0.3,
+    },
+    yaxis: {
+        min: 0,
+    },
+    colors: ['#DCE6EC'],
+};
+
+const sparkline3 = new ApexCharts(document.querySelector('#sparkline-3'), option_sparkline3);
+sparkline3.render();
+
+/*Color Mode Toggler Start*/
+(() => {
+    "use strict";
+
+    const storedTheme = localStorage.getItem("theme");
+
+    const getPreferredTheme = () => {
+        if (storedTheme) {
+            return storedTheme;
+        }
+
+        return window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light";
+    };
+
+    const setTheme = function (theme) {
+        if (
+            theme === "auto" &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+        ) {
+            document.documentElement.setAttribute("data-bs-theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-bs-theme", theme);
+        }
+    };
+
+    setTheme(getPreferredTheme());
+
+    const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector("#bd-theme");
+
+        if (!themeSwitcher) {
+            return;
+        }
+
+        const themeSwitcherText = document.querySelector("#bd-theme-text");
+        const activeThemeIcon = document.querySelector(".theme-icon-active i");
+        const btnToActive = document.querySelector(
+            `[data-bs-theme-value="${theme}"]`
+        );
+        const svgOfActiveBtn = btnToActive.querySelector("i").getAttribute("class");
+
+        for (const element of document.querySelectorAll("[data-bs-theme-value]")) {
+            element.classList.remove("active");
+            element.setAttribute("aria-pressed", "false");
+        }
+
+        btnToActive.classList.add("active");
+        btnToActive.setAttribute("aria-pressed", "true");
+        activeThemeIcon.setAttribute("class", svgOfActiveBtn);
+        const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+        themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
+
+        if (focus) {
+            themeSwitcher.focus();
+        }
+    };
+
+    window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", () => {
+            if (storedTheme !== "light" || storedTheme !== "dark") {
+                setTheme(getPreferredTheme());
+            }
+        });
+
+    window.addEventListener("DOMContentLoaded", () => {
+        showActiveTheme(getPreferredTheme());
+
+        for (const toggle of document.querySelectorAll("[data-bs-theme-value]")) {
+            toggle.addEventListener("click", () => {
+                const theme = toggle.getAttribute("data-bs-theme-value");
+                localStorage.setItem("theme", theme);
+                setTheme(theme);
+                showActiveTheme(theme, true);
+            });
+        }
+    });
+})();
+/*Color Mode Toggler End*/
